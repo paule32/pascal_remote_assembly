7a72636f885ffa6a4f39064a050f17a0bc5e88c8
Jens Kallup <kallup-dev@web.de>
2024-01-02 04:55:28 +0100

    update

--- src/BoostServer.cc
+++ src/BoostServer.cc
@@ -44,7 +44,97 @@
  *
  * Bei der Verwendung darf kein kommerzieller Zweck der Gewinnerzielung entstehen !
  *
- * \subsection license2 Geltungsbereich
+ * Freeware und Shareware-Programme zeichnen sich vor allem dadurch aus, dass sie
+ * kostenlos beziehungsweise gegen einen relativ geringen Preis dem Nutzer die
+ * Verwendung teilweise hochwertiger Computerprogramme ermöglichen, die sich durchaus
+ * mit kommerziellen Produkten messen lassen können. Daher sind diese Softwaretypen
+ * gerade in Zeiten begrenzter finanzieller Mittel - auch im schulischen Umfeld sehr
+ * beliebt und kommen in vielfältigen Gebieten zum Einsatz (zum Beispiel bei der
+ * Netzwerkadministration, bei der Einrichtung von Servern, bei der Installation von
+ * Internet-Software auf den Nutzer-PCs oder bei Office-Anwendungen).
+ *
+ * Da es sich auch bei Free- und Sharewareprogrammen um Computerprogramme handelt,
+ * die gemäß § 69a Urheberrechtsgesetz (UrhG) urheberrechtlich geschützt sind, liegt
+ * es bei diesen Softwareprodukten weitgehend in den Händen des Rechteinhabers
+ * zum Beispiel des Programmierers - zu bestimmen, in welchem Umfang diese durch dritte
+ * Personen genutzt werden dürfen.
+ *
+ * Dabei können Freeware- und Sharewareprogramme nach den Lizenzbedingungen in der Regel
+ * beliebig kopiert und weitergegeben werden, dagegen ist vor allem eine Veränderung der
+ * Programme üblicherweise nicht gestattet. Es ist deshalb eigentlich auch nicht korrekt,
+ * wenn im Zusammenhang mit Free- und Shareware immer wieder der Begriff der
+ * "Public-Domain-Software" verwendet wird, der frei übersetzt "Software, die im öffentlichen
+ * Eigentum steht" bedeutet. Denn dies impliziert, dass die Software von der Öffentlichkeit
+ * beliebig genutzt und damit auch verändert werden darf (also gemeinfrei ist); letzteres ist
+ * bei Free- und Shareware aber gerade nicht der Fall.
+ *
+ * \subsection license12 Welche Arten von Shareware gibt es ?
+ * Shareware kann man in zahlreiche Unterkategorien einteilen. So kann Shareware beispielsweise
+ * werbegestützte Software oder “Adware” sein, welche dem User Werbung anzeigen soll. Dies hat
+ * den Zweck Einnahmen für den Eigentümer zu generieren. Eine weitere Art bezeichnet man als
+ * Demoware, wobei es sich hier nur um eine Demoversion der Software handelt.
+ *
+ * Dabei ist es oft eine Testversion mit einer festgelegten Zeitspanne (“Trialware”).
+ * In anderen Fällen kann die gesamte Funktionalität der Anwendung deaktiviert sein, so dass man
+ * zwar alle Funktionen sehen kannst, aber dafür bezahlen muss (“Crippleware”).
+ *
+ * Weiterhin gibt es auch “Freemium”-Shareware, bei welcher einige Funktionen in der kostenlosen
+ * Version verfügbar sind, Sie diese jedoch bezahlen müssen, um die volle Funktionalität
+ * freischalten zu können.
+ *
+ * Obwohl Shareware eine gute Option für jeden ist, der Software testen möchte, bevor man sich zu
+ * einem Kauf verpflichtet, sollten Sie dennoch vorsichtig sein.
+ * Denn Cyberkriminelle sind nur allzu bereit, den Eifer der Menschen auszunutzen, etwas gratis
+ * zu bekommen.
+ *
+ * Dieses Versprechen freier Software ist eine gängige Social-Engineering-Taktik, mit der sie
+ * Internetnutzer dazu bringen, bösartige Software herunterzuladen.
+ *
+ * Bei Shareware gibt es ebenso wie bei anderen Softwareprodukten keine Garantie dafür, dass
+ * die Software virenfrei ist. Aus diesem Grund ist es wichtig, dass der Benutzer sein Antivirenprogramm
+ * auf dem aktuellen Stand hält, bevor er Shareware herunterlädt.
+ *
+ * Prinzipiell kann jeder Shareware herunterladen, solange er einen Computer hat, der die
+ * Systemanforderungen des Programms erfüllt. Es gibt jedoch einige Seiten, die nur Benutzern in
+ * bestimmten Ländern das Herunterladen von Shareware erlauben.
+ *
+ * Nicht alle Shareware-Programme werden überall zum Kauf angeboten. Der beste Weg, um zu sehen,
+ * wo man ein bestimmtes Programm kaufen kann, ist, die Website des Entwicklers zu besuchen und
+ * herauszufinden, auf welchen Plattformen das Programm erhältlich ist.
+ *
+ * Freeware ist Software, die kostenlos heruntergeladen werden kann, aber keine Lizenzgebühren
+ * mehr verlangt. Im Gegensatz dazu erfordert Shareware normalerweise eine Lizenzgebühr für den
+ * vollen Zugang zu allen Funktionen. Beide Modelle ermöglichen es Benutzern, Software kostenlos
+ * auszuprobieren, aber Shareware ist normalerweise etwas umfangreicher und bietet mehr Funktionen.
+ *
+ * Dieses Software-Produkt besitzt eine Hybrid-Lizenz zwischen Free- und Share-Ware.
+ * Sie können es kostenlos einsetzen und an andere Personen weitergeben, solange Sie keine Teile
+ * der Software ändern oder kopieren.
+ * Es bedarf einer schriftlichen Einwilligung des Entwicklers (Jens Kallup), sollten Teile geändert
+ * oder übernommen werden.
+ *
+ * Sie können dieses Produkt auf mehreren lokalen Computer gleichzeitig nutzen.
+ * Eine öffentlich zugängliche Nutzung ist nicht bestrebt mit der aktuellen Version.
+ *
+ * Obwohl bei der Entwicklung dieser Software viel Sorgfalt gepflegt wurde, können Fehler nicht
+ * ausgeschlossen werden.
+ *
+ * Es werden daher keine Garantien auf entstanden Schäden oder Kosten übernommen, die
+ * während der Verwendung dieser Software entstehen. 
+ * Alles erfolgt auf Eigenes Risiko !!!
+ *
+ * Für zukünftige Entwicklungen können Sie jedoch einen kleinen Obolus in Form von Spenden
+ * hinterlegen.
+ *
+ * \subsection license2 Impressum
+ * Für schriftliche Kommunikation können Sie die untenstehende Adresse verwenden.<br>
+ * Bitte legen Sie Ihren Schreiben einen ausreichend frankierten Rückumschlag bei - vielen Dank.
+ *
+ * Jens Kallup <br>
+ * Im Entenbad 2 <br>
+ * Haus 2 <br>
+ * 99820 Behringen <br>
+ * Thüringen - Deutschland <br>
  * \endcond
  *
  * \cond english
